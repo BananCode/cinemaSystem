ad012533656b65abd1306511b4b5bcee8d1ce0f1
Popovych <popovichtanja86@gmail.com>
2025-06-29 22:13:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "menu.h"
+#include "film.h"
+#include "hall.h"
+#include "session.h"
+#include "ticket.h"
+#include "colors.h"
 
+int main()
+{
+	int choice;
+	do
+	{
+		showMenu();
+		choice = getUserChoice();
+
+		switch (choice)
+		{
+		case 1:
+			std::cout << "лввввв\n";
+			break;
+		default:
+			std::cout << RED << "Помилка! Ви вибрали не вірну опцію повторіть знову\n" << RESET;
+			break;
+		}
+	} while (choice != 0);
+
+	return 0;
+}
