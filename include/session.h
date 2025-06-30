48acfaf6b94bb1a6f2938c102d435988e4e79a72
Popovych <popovichtanja86@gmail.com>
2025-06-30 11:27:41 +0300

    Update session.h

--- include/session.h
+++ include/session.h
@@ -1,1 +1,17 @@
+#ifndef SESSION_H
+#define SESSION_H
 
+#include <string>
+
+struct Film;
+struct Hall;
+
+struct Session
+{
+	int id;
+	Film* film;          
+	Hall* hall;          
+	std::string time;    
+};
+
+#endif
