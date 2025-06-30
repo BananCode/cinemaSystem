5167cde012fed96948137fadaaafecb784021f61
Popovych <popovichtanja86@gmail.com>
2025-06-30 11:26:48 +0300

    Update film.h

--- include/film.h
+++ include/film.h
@@ -1,1 +1,22 @@
+#ifndef FILM_H
+#define FILM_H
 
+#include <string>
+#include <vector>
+
+struct Film
+{
+    int id;               
+    std::string name;     
+    std::string genre;    
+};
+
+extern std::vector<Film> films;
+
+void addFilm(std::vector<Film>& films);                 
+void showFilms(std::vector<Film>& films);               
+void findFilm(std::vector<Film>& films);                
+void editFilmById(std::vector<Film>& films, int id);    
+void removeFilmById(std::vector<Film>& films, int id);  
+
+#endif
