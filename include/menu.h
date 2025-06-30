0e723ddb754e48abd89c5a66a8d661c679e7128a
Popovych <popovichtanja86@gmail.com>
2025-06-30 11:24:55 +0300

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -1,7 +1,11 @@
 #ifndef MENU_H
 #define MENU_H
 
-void showMenu();
 int getUserChoice();
+void mainMenu();
+void manageFilmsMenu();
+void manageHallMenu();
+void manageSessionMenu();
+void manageTicketsMenu();
 
 #endif
