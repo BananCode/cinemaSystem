218a683175040ad7f04b37c3bf28e132902d102a
Ivan <popovichtanja86@gmail.com>
2025-07-16 23:51:15 +0300

    Додано обробку некоректного введення, показ в формі таблиці та демо версію

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,26 @@
 #include "hall.h"
 #include "session.h"
 #include "ticket.h"
+#include "demo.h"
+#include "dataView.h"
 #include "colors.h"
 
 int main()
 {
+	std::vector<Film> films;
+	std::vector<Hall> halls;
+	std::vector<Session> sessions;
+	std::vector<Ticket> tickets;
 	int choice;
+	
 	do
 	{
 		mainMenu();
-		std::cin >> choice;
+		while (!(std::cin >> choice)) {											// Перевіряємо, чи введено число
+			std::cout << RED << "Помилка! Введіть ціле число: " << RESET;
+			std::cin.clear();                                                   // Очищаємо стан помилки
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаємо буфер
+		}
 
 		switch (choice)
 		{
@@ -28,14 +39,25 @@ int main()
 		case 4:
 			manageTicketsMenu();
 			break;
+		case 5:
+			break;
+		case 6:
+			showAllData(films, halls, sessions, tickets);
+			break;
+		case 7:
+			loadDemoData(films, halls, sessions, tickets);
+                std::cout << CYAN << "\nДемо-дані завантажено." << RESET << std::endl;
+			break;
 		case 0:
-			std::cout << GREEN;
-			std::cout << "Вихід з програми... До зустрічі 👋\n";
+			std::cout << YELLOW;
+			std::cout << "\nВихід з програми... До зустрічі 👋\n";
 			std::cout << "Гарного дня!\n";
 			std::cout << RESET;
 			break;
 		default:
-			std::cout << RED << "Невірний вибір. Спробуйте ще раз.\n";
+			std::cout << RED;
+			std::cout << "Невірний вибір. Спробуйте ще раз.\n";
+			std::cout << RESET;
 			break;
 		}
 	} while (choice != 0);
