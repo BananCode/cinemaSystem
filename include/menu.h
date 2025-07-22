14150601ecd1d5b06169f7b6d192ee043f21d80b
Popovych <popovichtanja86@gmail.com>
2025-07-22 15:18:58 +0300

    Update menu.h

--- include/menu.h
+++ include/menu.h
@@ -1,11 +1,16 @@
 #ifndef MENU_H
 #define MENU_H
 
-int getUserChoice();
+#include <vector>
+#include "film.h"
+#include "hall.h"
+#include "session.h"
+#include "ticket.h"
+
 void mainMenu();
-void manageFilmsMenu();
-void manageHallMenu();
-void manageSessionMenu();
-void manageTicketsMenu();
+void manageFilmsMenu(std::vector<Film>& films);
+void manageHallMenu(std::vector<Hall>& halls);
+void manageSessionMenu(std::vector<Session>& sessions);
+void manageTicketsMenu(std::vector<Ticket>& tickets, std::vector<Session>& sessions);
 
 #endif
