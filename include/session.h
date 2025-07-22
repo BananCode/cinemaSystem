238f53b5ccb4ac2d8e969a96869db4a2be9767ce
Popovych <popovichtanja86@gmail.com>
2025-07-22 15:18:40 +0300

    Update session.h
    Додано функцію addSession

--- include/session.h
+++ include/session.h
@@ -2,16 +2,23 @@
 #define SESSION_H
 
 #include <string>
+#include <vector>
+#include "film.h"
+#include "hall.h"
 
 struct Film;
 struct Hall;
 
 struct Session
 {
-	int id;
-	Film* film;          
-	Hall* hall;          
-	std::string time;    
+	int sessionId;
+	Film* movie;                // Вказівник на фільм, що показується на цьому сеансі
+	Hall* hall;                 // Вказівник на зал, де проводиться сеанс
+	std::string sessionTime;    // Час проведення сеансу у форматі строки
 };
+
+extern std::vector<Session> sessions;
+
+void addSession(std::vector<Session>& sessions);
 
 #endif
