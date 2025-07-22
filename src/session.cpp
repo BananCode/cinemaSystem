54f1144785038ed439c0608032aca6666d0edd54
Popovych <popovichtanja86@gmail.com>
2025-07-22 15:16:57 +0300

    Update session.cpp
    Додано функцію для додавання сеансу

--- src/session.cpp
+++ src/session.cpp
@@ -1,1 +1,84 @@
+#include <iostream>
 #include "session.h"
+#include "film.h"
+#include "hall.h"
+#include "colors.h"
+
+std::vector<Session> sessions;
+
+void addSession(std::vector<Session>& sessions)
+{
+    Session newSession;
+    bool isUnique = false;
+    bool found = false;
+    int filmId;
+    int hallId;
+
+    if (films.empty() || halls.empty())
+    {
+        std::cout << RED << "Помилка: немає фільмів або залів!" << RESET << std::endl;
+        return;
+    }
+
+	std::cin.ignore(); 
+	std::cout << "\nДодайте сеанс:\n";
+	std::cout << "Введіть ID сеансу: ";
+
+    while (!isUnique)
+    {
+        std::cin >> newSession.sessionId;
+        isUnique = true;
+
+        for (int i = 0; i < sessions.size(); i++)
+            if (newSession.sessionId == sessions[i].sessionId)
+            {
+                std::cout << RED;
+                std::cout << "\nЦей ID вже існує введіть айді ще раз: ";
+                std::cout << RESET;
+                isUnique = false;
+                break;
+            }
+    }
+
+    showFilms(films);
+    std::cout << "Виберіть ID фільму: ";
+    std::cin >> filmId;
+
+    for (int i = 0; i < films.size(); i++)
+        if (films[i].filmId == filmId)
+        {
+            newSession.movie = &films[i];
+            found = true;
+        }
+
+    if (!found)
+    {
+        std::cout << RED << "Фільм не знайдено!" << RESET << std::endl;
+        return;
+    }
+
+    showHall(halls);
+    std::cout << "Виберіть ID залу: ";
+    std::cin >> hallId;
+    found = false;
+
+    for (int i = 0; i < halls.size(); i++)
+        if (halls[i].hallId == hallId)
+        {
+            newSession.hall = &halls[i];
+            found = true;
+        }
+
+    if (!found)
+    {
+        std::cout << RED << "Зал не знайдено!" << RESET << std::endl;
+        return;
+    }
+    
+    std::cout << "Введіть час сеансу (наприклад, 2025-07-17 18:00): ";
+    std::cin.ignore();
+    std::getline(std::cin, newSession.sessionTime);
+
+    sessions.push_back(newSession);
+    std::cout << GREEN << "Сеанс додано!" << RESET << std::endl;
+}
