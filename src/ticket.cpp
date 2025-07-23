3777ad025eb9eb17fa28b087986cba2d30f0aabc
Popovych <popovichtanja86@gmail.com>
2025-07-23 14:39:24 +0300

    Update ticket.cpp
    Додано функцію showTickets

--- src/ticket.cpp
+++ src/ticket.cpp
@@ -23,3 +23,18 @@ void addTicket(std::vector<Ticket>& tickets, int ticketId, Session* session, int
     tickets.push_back(newTicket);
     std::cout << GREEN << "Квиток додано!" << RESET << std::endl;
 }
+
+void showTickets(std::vector<Ticket>& tickets)
+{
+    if (tickets.size() == 0)
+        std::cout << RED << "\nКвитків ще не має\n" << RESET;
+    else
+        for (int i = 0; i < tickets.size(); i++)
+        {
+            std::cout << "\nКвиток " << i + 1 << ": ";
+            std::cout << "\nАйді квитка: " << tickets[i].ticketId;
+            std::cout << "\nНомер місця квитка: " << tickets[i].seatNum;
+            std::cout << "\nЗал: " << tickets[i].session->hall->hallName;
+            std::cout << "\nНазва фільму: " << tickets[i].session->movie->filmName << "\n";
+        }
+}
