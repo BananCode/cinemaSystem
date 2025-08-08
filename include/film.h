eca397ecc60fe44b5c1f64c96a5b4905d0e34c2c
Popovych <popovichtanja86@gmail.com>
2025-08-08 11:43:09 +0300

    Update film.h
    додано enum з жанрами фільмів

--- include/film.h
+++ include/film.h
@@ -4,11 +4,25 @@
 #include <string>
 #include <vector>
 
+enum Genre 
+{
+    Comedy,
+    Drama,
+    Action,
+    Horror,
+    Fantasy,
+    Thriller,
+    Documentary,
+    GenreCount // Для підрахунку кількості жанрів
+};
+
+extern const std::string genreNames[GenreCount];
+
 struct Film
 {
     int filmId;               // Унікальний ідентифікатор фільму
-    std::string filmName;     // Назва фільму
-    std::string genre;        // Жанр фільму
+    std::string filmName;     // Назва фільму      
+    Genre genre;
 };
 
 extern std::vector<Film> films;
@@ -16,6 +30,6 @@ extern std::vector<Film> films;
 void addFilm(std::vector<Film>& films);                     // Додати новий фільм до вектора
 void showFilms(std::vector<Film>& films);                   // Вивести всі фільми у списку
 void editFilmById(std::vector<Film>& films);                // Редагувати фільм за його ID
-void removeFilmById(std::vector<Film>& films, int filmId);  // Видалити фільм зі списку за його ID
+void removeFilmById(std::vector<Film>& films);              // Видалити фільм зі списку за його ID
 
 #endif
