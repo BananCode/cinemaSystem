8444e520460e28c7712a8d7050c9256fa84999af
Popovych <popovichtanja86@gmail.com>
2025-08-11 14:23:55 +0300

    Update session.h
    додано функцію searchSessionsById

--- include/session.h
+++ include/session.h
@@ -17,11 +17,12 @@ struct Session
 	std::string sessionTime;    // Час проведення сеансу у форматі строки
 };
 
-extern std::vector<Session> sessions;
-
-void addSession(std::vector<Session>& sessions);
-void showSession(std::vector<Session>& sessions);
-void editSessionById(std::vector<Session>& sessions);
-void removeSessionById(std::vector<Session>& sessions);
+extern std::vector<Session> sessions; // Глобальний вектор сеансів
+           
+void addSession(std::vector<Session>& sessions);                  // Додавання нового сеансу
+void showSession(std::vector<Session>& sessions);                 // Вивід всіх сеансів
+void editSessionById(std::vector<Session>& sessions);             // Редагувати сеансу за ID
+void removeSessionById(std::vector<Session>& sessions);           // Видалити сеанс за ID
+void searchSessionsById(std::vector<Session>& sessions);		  // Пошук сеансів за ID
 
 #endif
