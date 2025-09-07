0b628a2029ff1c812fa63f5ea0f296a142add3a7
Popovych <popovichtanja86@gmail.com>
2025-09-07 11:49:05 +0300

    Update main.cpp
    changed interface language

--- src/main.cpp
+++ src/main.cpp
@@ -10,56 +10,62 @@
 
 int main()
 {
-	int mainMenuChoice;
-	
-	do
-	{
-		mainMenu();
-		while (!(std::cin >> mainMenuChoice)) 
-		{																		// Перевіряємо, чи введено число
-			std::cout << RED << "Помилка! Введіть ціле число: " << RESET;
-			std::cin.clear();                                                   // Очищаємо стан помилки
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Очищаємо буфер
-		}
+    int mainMenuChoice;
 
-		switch (mainMenuChoice)
-		{
-		case 1:
-			manageFilmsMenu(films);
-			break;
-		case 2:
-			manageHallMenu(halls);
-			break;
-		case 3:
-			manageSessionMenu(sessions);
-			break;
-		case 4:
-			manageTicketsMenu(tickets, sessions);
-			break;
-		case 5:
-			showAllData(films, halls, sessions, tickets);
-			break;
-		case 6:
-			loadDemoData(films, halls, sessions, tickets);
-                std::cout << CYAN << "\nДемо-дані завантажено.\n" << RESET;
-			break;
-		case 7:
-			saveAllData(films, halls, sessions, tickets);
-			std::cout << CYAN << "\nДані успішно збережено!\n" << RESET;
-			break;
-		case 8:
-			loadAllData(films, halls, sessions, tickets);
-			std::cout << CYAN << "\nДані успішно завантажено!\n" << RESET;
-			break;
-		case 0:
-			std::cout << YELLOW << "\nВихід з програми... До зустрічі 👋\n";
-			std::cout << "Гарного дня!\n" << RESET;
-			break;
-		default:
-			std::cout << RED << "Невірний вибір. Спробуйте ще раз.\n" << RESET;
-			break;
-		}
-	} while (mainMenuChoice != 0);
+    do
+    {
+        mainMenu();
+        while (true)
+        {
+            std::cin >> mainMenuChoice;
+            if (std::cin.fail())
+            {
+                std::cout << RED << "Error: Enter an integer!\n" << RESET;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            else
+                break;
+        }
 
-	return 0;
+        switch (mainMenuChoice)
+        {
+        case 1:
+            manageFilmsMenu(films);
+            break;
+        case 2:
+            manageHallMenu(halls);
+            break;
+        case 3:
+            manageSessionMenu(sessions);
+            break;
+        case 4:
+            manageTicketsMenu(tickets, sessions);
+            break;
+        case 5:
+            showAllData(films, halls, sessions, tickets);
+            break;
+        case 6:
+            loadDemoData(films, halls, sessions, tickets);
+            std::cout << CYAN << "\nDemo data loaded successfully.\n" << RESET;
+            break;
+        case 7:
+            saveAllData(films, halls, sessions, tickets);
+            std::cout << CYAN << "\nData saved successfully!\n" << RESET;
+            break;
+        case 8:
+            loadAllData(films, halls, sessions, tickets);
+            std::cout << CYAN << "\nData loaded successfully!\n" << RESET;
+            break;
+        case 0:
+            std::cout << YELLOW << "\nExiting program... Goodbye 👋\n";
+            std::cout << "Have a nice day!\n" << RESET;
+            break;
+        default:
+            std::cout << RED << "Invalid choice. Try again.\n" << RESET;
+            break;
+        }
+    } while (mainMenuChoice != 0);
+
+    return 0;
 }
